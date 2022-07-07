b2d2b6ded8edfdc2c32ca58df05198dc3f6fdd5d
ohjun99 <ohjun1999@gmail.com>
2022-07-07 21:36:52 +0900

    2022_07_07_파이어베이스_수첩_데이터_갯수_제한_추가_완료

--- app/src/main/java/com/example/moilsurok/activity/LoginActivity.kt
+++ app/src/main/java/com/example/moilsurok/activity/LoginActivity.kt
@@ -1,12 +1,10 @@
 package com.example.moilsurok.activity
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.moilsurok.DataClassUser
-import com.example.moilsurok.ListViewModel
 import com.example.moilsurok.adapter.ListAdapter
 import com.example.moilsurok.databinding.ActivityLoginBinding
 
@@ -14,8 +12,7 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     private lateinit var adapter: ListAdapter
-    private val viewModel by lazy { ViewModelProvider(this).get(ListViewModel::class.java) }
-    var userList = arrayListOf<DataClassUser>()
+
 
     val loginPhoneNumber = binding.loginPhoneNumber.toString()
 //    val userPhoneNumber = userList.phoneNumber
@@ -26,22 +23,11 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//
-//        binding.loginEnter.setOnClickListener {
-//            if (loginPhoneNumber == userPhoneNumber) {
-//                val intent = Intent(this, MainActivity::class.java)
-//                startActivity(intent)
-//
-//            }
-//        }
 
 
-        fun observerData() {
-            viewModel.fetchData().observe(this, Observer {
-                adapter.setListData(it)
-                adapter.notifyDataSetChanged()
-            })
 
-        }
+
+
+
     }
 }
--- app/src/main/java/com/example/moilsurok/activity/NoteActivity.kt
+++ app/src/main/java/com/example/moilsurok/activity/NoteActivity.kt
@@ -5,35 +5,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.TextView
 
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.moilsurok.DataClassUser
-import com.example.moilsurok.ListViewModel
 import com.example.moilsurok.R
-import com.example.moilsurok.adapter.ListAdapter
 import com.example.moilsurok.databinding.ActivityNoteBinding
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.auth.User
 
 
 class NoteActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNoteBinding
-    private lateinit var database: DatabaseReference
-    private lateinit var adapter:ListAdapter
-    private val viewModel by lazy { ViewModelProvider(this).get(ListViewModel::class.java) }
-    var userList = arrayListOf<DataClassUser>()
 
-    var firestore : FirebaseFirestore? = null
+
+    var firestore: FirebaseFirestore? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,19 +37,10 @@ class NoteActivity : AppCompatActivity() {
         binding.noteRecyclerView.adapter = NoteAdapter()
         binding.noteRecyclerView.layoutManager = LinearLayoutManager(this)
 
-//        val recyclerView : RecyclerView = findViewById(R.id.noteRecyclerView)
-//
-//
-//        adapter = ListAdapter(this, userList)
-//
-//        for (i in 0..20) {
-//            userList.add(DataClassUser("" + "기", "" + "님", "", "","","","","","","",""))
-//            //어떻게 데이터 값을 받을 것인지
-//        }
-//        recyclerView.layoutManager = LinearLayoutManager(this)
-//        recyclerView.setHasFixedSize(true)
-//        recyclerView.adapter = adapter
-//        observerData()
+
+
+
+
 
         binding.backKey.setOnClickListener {
             finish()
@@ -78,39 +56,38 @@ class NoteActivity : AppCompatActivity() {
             binding.menuBtn2.visibility = View.GONE
 
         }
-//        val sYear = resources.getStringArray(R.array.itemList)
-//        val sAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, sYear)
-//        binding.searchYear.adapter = sAdapter
-//
-//        val position = resources.getStringArray(R.array.itemList)
-//        val pAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, position)
-//        binding.searchPosition.adapter = pAdapter
-
 
 
     }
 
-    inner class NoteAdapter :  RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    inner class NoteAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-        var deNote : ArrayList<UserDataClass> = arrayListOf()
+        private val VIEW_TYPE_ITEM = 0
+        private val VIEW_TYPE_LOADING = 1
+
+
+        var deNote: ArrayList<UserDataClass> = arrayListOf()
 
         init {
-            firestore?.collection("teams")?.document("FxRFio9hTwGqAsU5AIZd")?.collection("User")?.addSnapshotListener{ querySnapshot, firebaseFirestoreException ->
-                // ArrayList 비워줌
-                deNote.clear()
+            firestore?.collection("teams")?.document("FxRFio9hTwGqAsU5AIZd")?.collection("User")?.orderBy("name")?.limit(20)
+                ?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                    // ArrayList 비워줌
+                    deNote.clear()
 
-                for (snapshot in querySnapshot!!.documents) {
-                    var item = snapshot.toObject(UserDataClass::class.java)
-                    deNote.add(item!!)
+                    for (snapshot in querySnapshot!!.documents) {
+                        var item = snapshot.toObject(UserDataClass::class.java)
+                        deNote.add(item!!)
+                    }
+                    notifyDataSetChanged()
+
                 }
-                notifyDataSetChanged()
-
-            }
         }
+
 
         // xml 파일을 inflate 하여 ViewHolder 를 생성
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            var view = LayoutInflater.from(parent.context).inflate(R.layout.item_note, parent, false)
+            var view =
+                LayoutInflater.from(parent.context).inflate(R.layout.item_note, parent, false)
             return ViewHolder(view)
         }
 
@@ -130,18 +107,18 @@ class NoteActivity : AppCompatActivity() {
 
             holder.itemView.setOnClickListener {
                 val intent = Intent(holder.itemView?.context, NoteProfileDetailActivity::class.java)
-                intent.putExtra("content","원하는 데이터를 보냅니다.")
-                intent.putExtra("year",user.year)
-                intent.putExtra("name",user.name)
-                intent.putExtra("birthDate",user.birthDate)
-                intent.putExtra("phoneNum",user.phoneNum)
-                intent.putExtra("email",user.email)
-                intent.putExtra("company",user.company)
-                intent.putExtra("department",user.department)
-                intent.putExtra("comPosition",user.comPosition)
-                intent.putExtra("comTel",user.comTel)
-                intent.putExtra("comAdr",user.comAdr)
-                intent.putExtra("faxNum",user.faxNum)
+                intent.putExtra("content", "원하는 데이터를 보냅니다.")
+                intent.putExtra("year", user.year)
+                intent.putExtra("name", user.name)
+                intent.putExtra("birthDate", user.birthDate)
+                intent.putExtra("phoneNum", user.phoneNum)
+                intent.putExtra("email", user.email)
+                intent.putExtra("company", user.company)
+                intent.putExtra("department", user.department)
+                intent.putExtra("comPosition", user.comPosition)
+                intent.putExtra("comTel", user.comTel)
+                intent.putExtra("comAdr", user.comAdr)
+                intent.putExtra("faxNum", user.faxNum)
                 ContextCompat.startActivity(holder.itemView.context, intent, null)
 
             }
@@ -149,7 +126,7 @@ class NoteActivity : AppCompatActivity() {
 
         // 리사이클러뷰의 아이템 총 개수 반환
         override fun getItemCount(): Int {
-          return deNote.size
+            return deNote.size
         }
 
         inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -165,10 +142,5 @@ class NoteActivity : AppCompatActivity() {
 
     }
 
-//    fun observerData(){
-//        viewModel.fetchData().observe(this, Observer {
-//            adapter.setListData(it)
-//            adapter.notifyDataSetChanged()
-//        })
-//    }
+
 }
